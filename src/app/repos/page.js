4c82552e2d8899074526db8d44fb6c5a1a708d52
Roberto Da Silva <psyrd5@nottingham.ac.uk>
2024-11-25 13:19:05 +0000

    Feat(Layout): Created grid layout and added TextArea and Button components.

--- src/app/repos/page.js
+++ src/app/repos/page.js
@@ -1,5 +1,24 @@
-`use client`;
+"use client";
+
+import {Button, TextArea} from "@carbon/react";
 
 export default function RepoPage() {
-  return <div>REPO PAGE</div>;
+  return <div className={"grid_container"}>
+    <div className={"left_column"}>
+    </div>
+    <div>
+      <div className={"chat_section"}>
+      </div>
+      <TextArea className={"text_input_section"} labelText={"Enter text"}>
+
+      </TextArea>
+      <div className={"submit_button_section"}>
+        <Button id={"submit_button"}>
+          Submit
+        </Button>
+      </div>
+    </div>
+    <div className={"right_column"}>
+    </div>
+  </div>
 }
