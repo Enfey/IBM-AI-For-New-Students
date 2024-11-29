648a9a269ebf98cee399e45b15fcda872d5de160
Roberto Da Silva <psyrd5@nottingham.ac.uk>
2024-11-29 14:23:47 +0000

    Feat(Chat): Embedded the assistant into the chat page using custom lib. from IBM.

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,23 +1,19 @@
 "use client";
 
 import {Button, TextArea} from "@carbon/react";
+import {WebChatCustomElement} from '@ibm-watson/assistant-web-chat-react';
+
+const watsonAssistantChatOptions = {
+  integrationID: "fdc6b027-684b-49a3-a8c0-82e0c7c90c97", // The ID of this integration.
+  region: "eu-gb", // The region your integration is hosted in.
+  serviceInstanceID: "f0d47658-0d2a-4a9e-a2b0-454062c0bf3d", // The ID of your service instance.
+}
 
 export default function RepoPage() {
   return <div className={"grid_container"}>
     <div className={"left_column"}>
     </div>
-    <div>
-      <div className={"chat_section"}>
-      </div>
-      <TextArea className={"text_input_section"} labelText={"Enter text"}>
-
-      </TextArea>
-      <div className={"submit_button_section"}>
-        <Button id={"submit_button"}>
-          Submit
-        </Button>
-      </div>
-    </div>
+    <WebChatCustomElement className="middle_column" config={watsonAssistantChatOptions} />
     <div className={"right_column"}>
     </div>
   </div>
