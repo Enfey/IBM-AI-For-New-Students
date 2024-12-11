a66a2ffb675affc702f04f18f9c09eeeed34a8a6
Roberto Da Silva <psyrd5@nottingham.ac.uk>
2024-12-11 14:56:24 +0000

    Refactor(route.js): added .env variables using server directive

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,3 +1,5 @@
+"use server"
+
 import { NextResponse } from "next/server";
 
 const AssistantV1 = require('ibm-watson/assistant/v1');
@@ -6,9 +8,9 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 const assistant = new AssistantV1({
     version: "2024-11-15",
     authenticator: new IamAuthenticator({
-        apikey: "o2IoqbMZh97454-HsrIAUdA6WkqrIFhoTv3Irzauhd_I"
+        apikey: process.env.APIKEY
     }),
-    serviceUrl: "https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/f0d47658-0d2a-4a9e-a2b0-454062c0bf3d"
+    serviceUrl: process.env.APIURL
 });
 
 export async function POST(request) {
