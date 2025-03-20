1457feb623b7b38ef5e246dcea492da4b7f924de
Henry Xie <1506894634@qq.com>
2025-03-20 14:39:49 +0000

    Initialize the About page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,11 +2,13 @@
 
 import {useAuth} from "@/hooks/useAuth";
 import {useRouter} from "next/navigation";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function AboutPage() {
+
     const { isLoggedIn, isInitialised } = useAuth();
     const router = useRouter();
+    const [slideIndex, setSlideIndex] = useState(0)
 
     useEffect(() => {
         if (isInitialised && !isLoggedIn) {
@@ -14,13 +16,40 @@ export default function AboutPage() {
         }
     }, [isInitialised, isLoggedIn, router]);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSlideIndex((prevIndex) => (prevIndex + 1) % 3);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <>
-            {!isLoggedIn ? null : (
-                <div className="about-container">
-                    <h1>About Page</h1>
+        <div className="about-container">
+            <div className="slideshow-container">
+                <div className="mySlides fade" style={{ display: slideIndex === 0 ? 'block' : 'none' }} align="center">
+                    <div className="number-text">1 / 3</div>
+                    <img src="https://www.jyshare.com/wp-content/uploads/2017/01/img_mountains_wide.jpg" style={{ width: "100%", height: "20vh"}} />
+                    <div className="slider-text"></div>
                 </div>
-            )}
-        </>
+
+                <div className="mySlides fade" style={{ display: slideIndex === 1 ? 'block' : 'none' }} align="center">
+                    <div className="number-text">2 / 3</div>
+                    <img src="https://www.jyshare.com/wp-content/uploads/2017/01/img_fjords_wide.jpg" style={{ width: "100%", height: "20vh"}} />
+                    <div className="slider-text"></div>
+                </div>
+
+                <div className="mySlides fade" style={{ display: slideIndex === 2 ? 'block' : 'none' }} align="center">
+                    <div className="number-text">3 / 3</div>
+                    <img src="https://www.jyshare.com/wp-content/uploads/2017/01/img_nature_wide.jpg" style={{ width: "100%", height: "20vh"}} />
+                    <div className="slider-text"></div>
+                </div>
+            </div>
+            <div style={{ textAlign: "center" }}>
+                {[0, 1, 2].map((index) => (
+                    <span key={index} className={`dot ${index === slideIndex ? 'active' : ''}`}></span>
+                ))}
+            </div>
+        </div>
     );
 }
