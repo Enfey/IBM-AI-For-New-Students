745d3c005192ef1e61b79a2a4845e889baeff19a
Felix Riley-Kay <cleanblessing@protonmail.com>
2025-05-04 06:20:05 +0100

    Resolved merge conflict

--- src/app/api/create_session/route.js
+++ src/app/api/create_session/route.js
@@ -15,11 +15,7 @@ export const dynamic = "force-dynamic";
 
 
 const assistant = new AssistantV2({
-<<<<<<< HEAD
-    version: "2025-04-02",
-=======
     version: '2025-05-01',
->>>>>>> a6445437e2d3fae3107283ec9aa3d86c00d4eedf
     authenticator: new IamAuthenticator({
         // !!!!  Replace with your API key !!!!
         apikey: process.env.APIKEY,
@@ -29,27 +25,11 @@ const assistant = new AssistantV2({
 
 export async function GET(request) {
     try {
-<<<<<<< HEAD
-        if (session_id) {
-            console.log("Deleting old session!");
-            await assistant.deleteSession({
-                assistantId: "7b6004ec-9cf4-4631-a286-fac63052422d",
-                sessionId: session_id,
-            });
-        }
-
-        const res = await assistant.createSession({
-            assistantId: "7b6004ec-9cf4-4631-a286-fac63052422d",
-        });
-        session_id = res.result.session_id;
-        console.log("New session!");
-=======
 
         const res = await assistant.createSession({
             assistantId: '7b6004ec-9cf4-4631-a286-fac63052422d'
         })
         console.log("New session!")
->>>>>>> a6445437e2d3fae3107283ec9aa3d86c00d4eedf
 
         return NextResponse.json({
             payload: res.result.session_id,
